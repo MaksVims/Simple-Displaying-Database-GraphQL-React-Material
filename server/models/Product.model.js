a99ad1c2b9f2ade3ee6541a97da37c527ae6f353
Maksim Smetanin <maks.s923@mail.ru>
2021-10-20 18:28:58 +0500

    Fix product model
    Add required: true to quantity field

--- server/models/Product.model.js
+++ server/models/Product.model.js
@@ -2,7 +2,7 @@ const {Schema, model} = require('mongoose')
 
 const ProductSchema = new Schema({
   title: {type: String, required: true, unique: true},
-  quantity: {type: Number, default: 0},
+  quantity: {type: Number, required: true, default: 0},
   providerId: {type: String, required: true}
 })
 
