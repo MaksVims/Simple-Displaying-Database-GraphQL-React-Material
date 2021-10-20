0110c9ec8faf6dfc0e08e1be6a30c64cf721e468
Maksim Smetanin <maks.s923@mail.ru>
2021-10-20 20:41:34 +0500

    Create main color theme and connect to app

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createTheme, ThemeProvider} from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff7043',
+    },
+    secondary: {
+      main: '#6200ea',
+    },
+  },
+})
 
 ReactDOM.render(
-  <App/>
+  <ThemeProvider theme={theme}>
+    <App/>
+  </ThemeProvider>
   ,
   document.getElementById('root')
 );
