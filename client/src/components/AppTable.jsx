b4ab4c8c8b696d11e2708077016ac9cca9ec117a
Maksim Smetanin <maks.s923@mail.ru>
2021-10-21 11:51:51 +0500

    Create AppTable component

---
+++ client/src/components/AppTable.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {IconButton, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
+import {DeleteOutlined, SystemUpdateAltOutlined} from "@mui/icons-material";
+
+const AppTable = ({data, updateHandler, removeHandler, subItem}) => {
+  const fieldsName = Object.keys(data[0] || {})?.filter(field => field !== 'id' && field !== '__typename')
+
+  if (!fieldsName.length) {
+    return null
+  }
+
+  return (
+    <>
+      <TableHead sx={{width: '100%'}}>
+        <TableRow>
+          {fieldsName.map(field => (
+            <TableCell key={field}>{field}</TableCell>
+          ))}
+          <TableCell align={"center"}>Actions</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map(item => (
+          <TableRow key={item.id}>
+            {fieldsName.map(itemField => (
+              <React.Fragment key={itemField}>
+                {Array.isArray(item[itemField]) ?
+                  subItem(item[itemField]) :
+                  <TableCell key={itemField}>{item[itemField]}</TableCell>
+                }
+              </React.Fragment>
+              ))}
+            <TableCell align={"center"} sx={{width: '150px'}}>
+              <IconButton onClick={() => updateHandler(item)} sx={{mr: '20px'}}><SystemUpdateAltOutlined
+                color='success'/></IconButton>
+              <IconButton onClick={() => removeHandler(item)}><DeleteOutlined color='error'/></IconButton>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody></>
+  );
+};
+
+export default AppTable;
